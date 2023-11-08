ebdfcce9a10db321085b1f4d99ac80ccbd0c1f92
sebatustra <sebatustra@protonmail.com>
2023-11-08 20:50:44 -0300

    tests should be working

--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,24 @@
 use std::net::TcpListener;
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
+use sqlx::PgPool;
 
 use crate::routes::health_check;
 use crate::routes::subscribe;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-  let server = HttpServer::new( || {
-      App::new()
-          .route("/health_check", web::get().to(health_check))
-          .route("/subscriptions", web::post().to(subscribe))
-      })
-      .listen(listener)?
-      .run();
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool
+) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(  move|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
+        })
+        .listen(listener)?
+        .run();
 
-  Ok(server)
+    Ok(server)
 }
