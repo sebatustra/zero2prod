a49715b6cc5672e8c461ebbc27c670f0e2cdc782
sebatustra <sebatustra@protonmail.com>
2024-03-01 12:31:39 -0300

    better organization, added script for database initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+// cargo watch -x run    cargo watch -x check -x test -x run
+use zero2prod::startup::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000")
+    .expect("Failed to bind to port");
+
+    run(listener)?.await
 }
