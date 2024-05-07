1b30d02003f2a8cea1b6c6d1aec30bb0899a2b22
sebatustra <sebatustra@protonmail.com>
2024-05-07 17:41:12 -0400

    remoevd dotenv for security

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use std::net::TcpListener;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
-use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    dotenv().ok();
     let configuration = get_configuration().expect("Failed to read configuration");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
