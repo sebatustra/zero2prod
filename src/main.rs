11e927f4c7aba1d26dec64d6b3d1fd7903b8e2d8
sebatustra <sebatustra@protonmail.com>
2024-05-07 17:30:47 -0400

    page 69

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    
+    let listener = TcpListener::bind(address)
+        .expect("Failed to bind random port");
+
+    run(listener)?.await
 }
+
+// cargo watch -x check -x test -x run
