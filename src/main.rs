60ede029426c7fae04ff3325371e9d4bb3556b14
sebatustra <sebatustra@protonmail.com>
2024-05-11 12:27:25 -0400

    added telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use std::net::TcpListener;
-use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use sqlx::PgPool;
+use std::net::TcpListener;
+use secrecy:: ExposeSecret;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
+
+    let connection_pool = PgPool::connect(
+            &configuration.database.connection_string().expose_secret()
+        )
+        .await
+        .expect("Failed to connect to postgres");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     
     let listener = TcpListener::bind(address)
         .expect("Failed to bind random port");
 
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
 
 // cargo watch -x check -x test -x run
