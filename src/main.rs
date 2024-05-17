605b6a65851a13abc2bb5474bb47cf6f54449339
sebatustra <sebatustra@protonmail.com>
2024-05-17 15:34:11 -0400

    implemented new client, formatted tests and builder

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use zero2prod::startup::run;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
@@ -11,16 +9,11 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration");
 
-    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
+    let application = Application::build(configuration).await?;
 
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
+    application.run_until_stopped().await?;
 
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
-
-    run(listener, connection_pool)?.await
+    Ok(())
 }
 
 // cargo watch -x check -x test -x run
